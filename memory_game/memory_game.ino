ff2221af4da698bc9064a1ddbd9c9fb8b83f3dfa
IceHornet <f.ulmschneider@bluewin.ch>
2015-01-17 18:56:27 +0100

    Added score display, demo sequence

--- memory_game/memory_game.ino
+++ memory_game/memory_game.ino
@@ -9,6 +9,19 @@ void setup() {
   randomSeed(analogRead(2));
   
   swipe(80);
+  
+  for(int x = 0; x<20; x++) {
+  
+    show(x);
+    delay(500);
+  }
+  
+  blink(200);
+  delay(100);
+  blink(200);
+  
+  DDRB = 0xf0;
+  PORTB = 0xff;
 }
 
 
@@ -36,9 +49,10 @@ void swipe(int del) {
   PORTB = 0b00001110;
   delay(del);
   PORTB = 0b00001111;
+  delay(del);
 }
 
-void blink(del) {
+void blink(int del) {
   DDRB = 0xff;
   
   PORTB = 0b00000000;
@@ -46,3 +60,19 @@ void blink(del) {
   PORTB = 0b00001111;
 }
 
+void show(byte num) {
+  DDRB = 0xff;
+  if(num < 16) {
+    byte outputNum;
+    byte j=3;
+    //flip the first four bits
+    for (byte i=0; i<4; i++ ) {
+      bitWrite(outputNum, i, bitRead(num, j));
+      j--;
+    }
+    
+    PORTB = outputNum^0x0f;
+  }else {
+    PORTB = 0x0f;
+  }
+}
